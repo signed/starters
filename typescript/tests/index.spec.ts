7364528e6ff4e78f8b28cd1f25ec7a17e8c8deb1
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-11-16 20:44:14 +0100

    adjust file formatting

--- typescript/tests/index.spec.ts
+++ typescript/tests/index.spec.ts
@@ -1,7 +1,7 @@
 import { hello } from '../src/index'
 
 describe('hello world', () => {
-    it('should greet the parameter', () => {
-        expect(hello('you')).toEqual('Hello you');
-    });
+  it('should greet the parameter', () => {
+    expect(hello('you')).toEqual('Hello you');
+  });
 });
