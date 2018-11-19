4e32171a752e74574895ceca4ae3ed4fda96fde9
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-11-19 22:06:35 +0100

    add sample for async function

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,1 +1,8 @@
 export const hello = (name: string): string => `Hello ${name}`;
+
+
+export const asyncFunction = async (input: string) => {
+  return new Promise<string>((resolve, reject) => {
+    resolve(`${input} resolve`);
+  });
+};
--- typescript/tests/index.spec.ts
+++ typescript/tests/index.spec.ts
@@ -1,7 +1,14 @@
-import { hello } from '../src/index'
+import { hello, asyncFunction } from '../src/index'
 
 describe('hello world', () => {
   it('should greet the parameter', () => {
     expect(hello('you')).toEqual('Hello you');
+  });
+});
+
+
+describe('testing async functions', () => {
+  it('await with an async test function', async () => {
+    expect(await asyncFunction('123')).toEqual('123 resolve')
   });
 });
