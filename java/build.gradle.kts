cd4c7b1509b9ca7ec52ac42913d4ccdc27db9749
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-03-26 23:52:04 +0100

    update dependencies

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.github.ben-manes.versions") version "0.20.0"
+    id("com.github.ben-manes.versions") version "0.21.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.9"
     java
 }
@@ -10,8 +10,8 @@ repositories {
 }
 
 dependencies {
-    "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.3.1")
-    "testImplementation"("org.junit.jupiter:junit-jupiter-params:5.3.1")
-    "testImplementation"("org.assertj:assertj-core:3.11.1")
-    "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+    "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.5.0-M1")
+    "testImplementation"("org.junit.jupiter:junit-jupiter-params:5.5.0-M1")
+    "testImplementation"("org.assertj:assertj-core:3.12.2")
+    "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.5.0-M1")
 }
