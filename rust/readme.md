a7a72ad75eb9ef852c1e95c9c5edf0da4b4082b9
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-08-17 16:12:15 +0200

    basic rust setup

---
+++ rust/readme.md
@@ -0,0 +1,24 @@
+https://rustup.rs/
+```sh
+brew install rustup-init
+rustup-init
+rustup component add rust-src
+cargo init rust
+```
+
+undo everything
+```sh
+rustup self uninstall
+```
+
+# Reading
+https://sourabhbajaj.com/mac-setup/Rust/
+https://doc.rust-lang.org/rust-by-example/hello.html
+https://doc.rust-lang.org/stable/rust-by-example/
+
+https://doc.rust-lang.org/stable/rust-by-example/cargo.html
+https://doc.rust-lang.org/cargo/getting-started/first-steps.html
+https://intellij-rust.github.io/docs/troubleshooting.html
+https://doc.rust-lang.org/std/
+
+https://crates.io/
---
+++ rust/src/main.rs
@@ -0,0 +1,52 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle { width: 8, height: 7 };
+        let smaller = Rectangle { width: 5, height: 1 };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle { width: 8, height: 7 };
+        let smaller = Rectangle { width: 5, height: 1 };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn assert_checks_for_true() {
+        assert!(true);
+    }
+
+    #[test]
+    #[ignore]
+    fn fail_a_test() {
+        panic!("Make this test fail");
+    }
+}
