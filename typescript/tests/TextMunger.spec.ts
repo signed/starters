2ce0776ecc5a38f755d2bbd16ef39f1eb538d6dc
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-08-26 16:11:12 +0200

    first refactoring

--- typescript/tests/TextMunger.spec.ts
+++ typescript/tests/TextMunger.spec.ts
@@ -6,11 +6,43 @@ Whitespace, punctuation, numbers -- anything that isn't a word -- should also re
 TDD it :)
  */
 
+/*
+Drivers for changing the production code
+- failing tests
+- duplication
+- refactoring
+ */
+
+/*
+https://stanislaw.github.io/2016/01/25/notes-on-test-driven-development-by-example-by-kent-beck.html
+strategies to get a test green as quick as possible
+1. Fake it
+1. Obvious implementation
+1.
+ */
+
+/*
+The cycle http://www.growing-object-oriented-software.com/figures.html
+ */
+
+/*
+Refactoring techniques
+1. Parallel Change
+ */
+
+function reverse(string: string) {
+  return string.split('').reverse().join('');
+}
+
 function munge(text: string) {
-  if ('abcd' === text) {
-    return 'acbd'
+  if (text.length < 4) {
+    return text;
   }
-  return text;
+  let first = text[0];
+  let last = text[text.length - 1];
+  let center = text.slice(1, text.length - 1);
+  let reversedCenter = reverse(center);
+  return first + reversedCenter + last
 }
 
 describe('text munger', () => {
