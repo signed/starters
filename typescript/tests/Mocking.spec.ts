70d80718766ae7ff41ca99cec82a29295c9ec151
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-10-30 20:32:06 +0100

    better name

--- typescript/tests/Mocking.spec.ts
+++ typescript/tests/Mocking.spec.ts
@@ -1,6 +1,6 @@
 const passErrorToCallback = (callback: (error: Error) => void) => callback(new Error('Secret Sauce'));
 
-test('should ', () => {
+test('message of error passed to the callback ', () => {
   const callback = jest.fn();
   passErrorToCallback(callback);
   const passedError = callback.mock.calls[0][0];
