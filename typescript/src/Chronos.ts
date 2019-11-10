068f62a67c87c38a7e9a50bf644a40028c92ad99
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-11-10 19:21:07 +0100

    samples for testing setTimeout different ways

---
+++ typescript/src/Chronos.ts
@@ -0,0 +1,10 @@
+type Callback = () => void;
+const OneSecond = 1000;
+export const deeplyNestedAsync = (one: Callback, two: Callback) => {
+  setTimeout(() => {
+    one();
+    setTimeout(() => {
+      two();
+    }, OneSecond);
+  }, OneSecond);
+};
