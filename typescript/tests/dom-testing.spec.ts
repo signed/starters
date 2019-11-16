a56286cc2863d25aa343adadda17f9466bd17a17
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-11-16 22:03:00 +0100

    add an example for testing-library/dom

--- typescript/tests/dom-testing.spec.ts
+++ typescript/tests/dom-testing.spec.ts
@@ -1,21 +1,34 @@
+// https://testing-library.com/docs/react-testing-library/cheatsheet
 import { fireEvent, queries } from '@testing-library/dom';
 import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
 
-
-// https://testing-library.com/docs/react-testing-library/cheatsheet
-document.body.innerHTML = '<input type="text" placeholder="hello-placeholder" value="catch"/>';
-
-function inputElement() {
-  return queries.getByPlaceholderText(document.body, 'hello-placeholder');
+function inputElement(): HTMLInputElement {
+  return queries.getByPlaceholderText(document.body, 'hello-placeholder') as HTMLInputElement;
 }
 
 describe('text input', () => {
+  beforeEach(() => {
+    document.body.innerHTML = '<input type="text" placeholder="hello-placeholder" value="initial"/>';
+  });
+
   it('read the initial value', () => {
-    expect(inputElement()).toHaveValue('catch')
+    expect(inputElement()).toHaveValue('initial');
   });
 
   it('change the initial value', () => {
     fireEvent.change(inputElement(), { target: { value: 'updated' } });
-    expect(inputElement()).toHaveValue('updated')
+    expect(inputElement()).toHaveValue('updated');
+  });
+
+  it('replaces instead of continue writing at the end', () => {
+    inputElement().addEventListener('keydown', (event: KeyboardEvent) => {
+      console.log(event.key);
+    });
+    //inputElement().setSelectionRange(2, 2);
+    userEvent.type(inputElement(), 'one');
+    console.log(inputElement().selectionStart)
+    userEvent.type(inputElement(), 'two');
+    expect(inputElement()).toHaveValue('two');
   });
 });
