465808cfcb4b0219920077a74d3116cd8854b88e
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-11-30 19:19:23 +0100

    refactor and more ideas

--- typescript/spa-pos-backlog.md
+++ typescript/spa-pos-backlog.md
@@ -1,14 +1,29 @@
+# background
+You are developing the pos terminal for a spa.
+A teller enters the package and payment method over a touch screen.
+Your task is to calculate the price a visitor as to pay.
+
+
 # ideas
 - reduced price because of maintenance
-- back pay 1,20 per 30 minutes 
-
-
+- back pay 1,20 per 30 minutes
+- renting towels and bathrobes
+  - people keep stealing 
+- gastronomy payment
+- pay 2h stay 4h coupon
+- recommendation about cheapest payment method
+- upfront paid for price reduction
+  100 €	10 % reduction
+  200 €	15 % reduction
+  500 €	20 % reduction
 
 # Backlog
-1. Basic pay
+1. Basic pay 
 - 2h 12 €
 - 4h 16 €
 - all day 18 €
+
+1. Upfront pay
 
 1. Early bird price
 
--- typescript/src/point-of-sales.spec.ts
+++ typescript/src/point-of-sales.spec.ts
@@ -1,21 +1,26 @@
-function calculatePriceFor(ticketType: number | 'dayticket') {
-  if (ticketType === 'dayticket') {
+type Package = 'day ticket' | '4 hours' | '2 hours';
+
+function calculatePriceFor(ticketType: Package) {
+  if (ticketType === 'day ticket') {
     return 18;
   }
-  if (ticketType === 4) {
+  if (ticketType === '4 hours') {
     return 16;
   }
-  return 12;
+  if (ticketType === '2 hours') {
+    return 12;
+  }
+  throw new Error('should never be reached')
 }
 
 it('two hours cost 12 euro ', () => {
-  expect(calculatePriceFor(2)).toBe(12);
+  expect(calculatePriceFor('2 hours')).toBe(12);
 });
 
 it('four hours cost 16 euro ', () => {
-  expect(calculatePriceFor(4)).toBe(16);
+  expect(calculatePriceFor('4 hours')).toBe(16);
 });
 
 it('a day ticket costs 18 euro ', () => {
-  expect(calculatePriceFor('dayticket')).toBe(18);
+  expect(calculatePriceFor('day ticket')).toBe(18);
 });
