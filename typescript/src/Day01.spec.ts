bc0d800b4cf4fd9528981877523a4bba6bec81de
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-12-04 00:34:09 +0100

    day one 2nd part of the challenge

--- typescript/src/Day01.spec.ts
+++ typescript/src/Day01.spec.ts
@@ -1,24 +1,53 @@
 import BigNumber from 'bignumber.js';
 import { readFileSync } from 'fs';
 
-const calculateFuelRequired = (mass: BigNumber) => {
-  const blup = mass.dividedBy(3).toNumber();
-  return Math.floor(blup) - 2;
+const naiveFullCalculator = (mass: BigNumber) => {
+  const fullRequired = Math.floor(mass.dividedBy(3).toNumber()) - 2;
+  if (fullRequired < 0) {
+    return 0;
+  }
+  return fullRequired;
 };
 
-test('day 01 challenge ', () => {
-  const input = readFileSync(__dirname +'/Day01.input.csv','utf8');
+const considerWeigtOfAddedFuel = (mass: BigNumber) => {
+  let currentMass = naiveFullCalculator(mass);
+  const masses = [];
+  while (currentMass != 0) {
+    masses.push(currentMass);
+    currentMass = naiveFullCalculator(new BigNumber(currentMass));
+  }
+  return masses.reduce((acc, cur) => acc + cur, 0);
+};
+
+type FuelCalculator = (mass: BigNumber) => number
+
+function fuelForVoyage(fuelCalculator: FuelCalculator) {
+  const input = readFileSync(__dirname + '/Day01.input.csv', 'utf8');
   const lines = input.split('\n').filter(line => line.trim());
-  const totalWeight = lines
+  return lines
     .map(line => new BigNumber(line))
-    .map(calculateFuelRequired)
+    .map(fuelCalculator)
     .reduce((acc, cur) => acc + cur, 0);
-  expect(totalWeight).toBe(3488702);
+}
+
+
+test('day 01 challenge ', () => {
+  expect(fuelForVoyage(naiveFullCalculator)).toBe(3488702);
+});
+
+test('day 01 challenge fuel has weight too ', () => {
+  expect(fuelForVoyage(considerWeigtOfAddedFuel)).toBe(5230169);
 });
 
 test('day one samples', () => {
-  expect(calculateFuelRequired(new BigNumber(12))).toBe(2);
-  expect(calculateFuelRequired(new BigNumber(14))).toBe(2);
-  expect(calculateFuelRequired(new BigNumber(1969))).toBe(654);
-  expect(calculateFuelRequired(new BigNumber(100756))).toBe(33583);
+  expect(naiveFullCalculator(new BigNumber(12))).toBe(2);
+  expect(naiveFullCalculator(new BigNumber(14))).toBe(2);
+  expect(naiveFullCalculator(new BigNumber(1969))).toBe(654);
+  expect(naiveFullCalculator(new BigNumber(100756))).toBe(33583);
+});
+
+test('day one fuel has mass too', () => {
+  expect(considerWeigtOfAddedFuel(new BigNumber(14))).toBe(2);
+  expect(considerWeigtOfAddedFuel(new BigNumber(1969))).toBe(966);
+  expect(considerWeigtOfAddedFuel(new BigNumber(100756))).toBe(50346);
 });
