a7cd1a2c201546db7f32c24c24fadf9dc92eea49
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-12-04 00:47:11 +0100

    remove sample tests

--- typescript/tests/Mocking.spec.ts
+++
@@ -1,8 +0,0 @@
-const passErrorToCallback = (callback: (error: Error) => void) => callback(new Error('Secret Sauce'));
-
-test('message of error passed to the callback ', () => {
-  const callback = jest.fn();
-  passErrorToCallback(callback);
-  const passedError = callback.mock.calls[0][0];
-  expect(passedError.message).toEqual('Secret Sauce')
-});
