268619e035f368750ec3e8242d26ac3c84a18a6d
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-12-17 22:37:05 +0100

    backup whats here

--- typescript/src/Day13.spec.ts
+++ typescript/src/Day13.spec.ts
@@ -1,5 +1,5 @@
 import { partition } from './arrays';
-import { loadIntProgramAt, runProgram } from './IntCodeComputer';
+import { IntCodeComputer, loadIntProgramAt, runProgram } from './IntCodeComputer';
 
 class Coordinate {
   constructor(readonly x: number, readonly y: number) {
@@ -24,12 +24,37 @@ enum Tile {
 
 type Banana = [Coordinate, Tile];
 
+function tileToString(tile: Tile | undefined) {
+  if (tile === undefined) {
+    return 'v';
+  }
+  switch (tile) {
+    case Tile.Ball:
+      return 'o';
+    case Tile.HorizontalPaddle:
+      return '_';
+    case Tile.Block:
+      return '#';
+    case Tile.Empty:
+      return ' ';
+    case Tile.Wall:
+      return '■';
+  }
+  throw new Error();
+}
+
 class Screen {
+
+  private bottomRight = new Coordinate(0, 0);
   readonly pixels = new Map<String, Tile>();
+  score = 0;
 
   display(banana: Banana) {
     const [coordinate, tile] = banana;
     this.pixels.set(coordinate.toString(), tile);
+    const newBottomX = Math.max(this.bottomRight.x, coordinate.x);
+    const newBottomY = Math.max(this.bottomRight.y, coordinate.y);
+    this.bottomRight = new Coordinate(newBottomX, newBottomY);
   }
 
   numberOfBlocks() {
@@ -40,16 +65,98 @@ class Screen {
       return acc;
     }, 0);
   }
+
+  toString(): string {
+    const lines: string[] = [];
+    for (let y = 0; y <= this.bottomRight.y; y++) {
+      const line: string[] = [];
+      for (let x = 0; x <= this.bottomRight.x; x++) {
+        const tile = this.pixels.get(new Coordinate(x, y).toString());
+        line.push(tileToString(tile));
+      }
+      lines.push(line.join(''));
+    }
+    return lines.join('\n');
+  }
 }
 
-const loadBreakout = () => runProgram(loadIntProgramAt('Day13'));
+const loadBreakout = () => loadIntProgramAt('Day13');
+
+const buildBanana = (group: number[]): Banana => [new Coordinate(group[0], group[1]), group[2] as Tile];
 
 it('day 13 part 1 ', () => {
-  const intCodeComputer = loadBreakout();
+  const intCodeComputer = runProgram(loadBreakout());
   const screen = new Screen();
   const numbers = intCodeComputer.output();
-  partition(numbers, 3).map<Banana>(group => [new Coordinate(group[0], group[1]), group[2] as Tile])
+  partition(numbers, 3).map<Banana>(group => buildBanana(group))
     .forEach(it => screen.display(it));
 
   expect(screen.numberOfBlocks()).toEqual(369);
+  console.log(screen.toString());
+});
+
+enum JoystickPosition {
+  Neutral = 0,
+  Left = -1,
+  Right = 1,
+}
+
+it('day 13 part 2', () => {
+  const screen = new Screen();
+  const program = loadBreakout();
+  program[0] = 2; // fake coin insert
+  const output: number [] = [];
+  const intCodeComputer = new IntCodeComputer();
+
+  let positionBall: Coordinate | undefined = undefined;
+  let positionPaddle: Coordinate | undefined = undefined;
+  let started = false;
+
+  intCodeComputer.load(program);
+  intCodeComputer.onOutput((out) => {
+    output.push(out);
+    if (output.length !== 3) {
+      return;
+    }
+    const banana = buildBanana(output);
+    screen.display(banana);
+    output.length = 0;
+
+    console.log(screen.toString());
+
+    const [coordinate, tile] = banana;
+    if (coordinate.x === -1) {
+      console.log('score: ' + tile);
+      started = true;
+      screen.score = tile;
+    }
+
+    if (tile === Tile.Ball) {
+      positionBall = coordinate;
+    }
+    if (tile === Tile.HorizontalPaddle) {
+      positionPaddle = coordinate;
+    }
+    if (positionBall === undefined || positionPaddle === undefined || !started) {
+      return;
+    }
+    const paddleX = positionPaddle.x;
+    const ballX = positionBall.x;
+    if (paddleX === ballX) {
+      console.log('_');
+      intCodeComputer.addInput([JoystickPosition.Neutral]);
+    } else if (paddleX < ballX) {
+      console.log('>');
+      intCodeComputer.addInput([JoystickPosition.Right]);
+    } else if (paddleX > ballX) {
+      console.log('<');
+      intCodeComputer.addInput([JoystickPosition.Left]);
+    }
+  });
+
+  while (!intCodeComputer.terminated()) {
+    intCodeComputer.execute();
+  }
+
+  expect(screen.score).toBe(34);
 });
