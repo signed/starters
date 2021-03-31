5e82ba6d60a429e60953e558838e94476d39cd46
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-03-31 22:20:39 +0200

    update dependencies

--- typescript/src/jest/todo.test.ts
+++ typescript/src/jest/todo.test.ts
@@ -6,3 +6,35 @@ describe('just want to keep a reminder to write a test', () => {
     expect(true).toBe(true);
   });
 });
+
+type Complex = {
+  one: string
+  timeLeft: number
+}
+
+type Present<T> = {
+  present: true;
+} & T
+
+type Absent = {
+  present: false;
+}
+
+type Box<T> = Present<T> | Absent
+
+const box = (complex?: Complex): Box<Complex> => {
+  if (complex=== undefined) {
+    return { present: false };
+  }
+  return { present: true, ...complex };
+}
+
+type Maybe<T> = void | T
+
+const some = (complex?: Complex) => {
+  const present = complex !== undefined;
+  //const derived = present ? complex.two : 45
+
+  const complexInBox = box(complex);
+  const warn = complexInBox.present ? complexInBox.timeLeft <= 3 : false
+}
