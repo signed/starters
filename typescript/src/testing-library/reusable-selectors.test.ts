bdd54e04c9c697494273030b361401dddb40c0b8
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-12-11 15:53:11 +0100

    add example for testing-library-selector

---
+++ typescript/src/testing-library/reusable-selectors.test.ts
@@ -0,0 +1,16 @@
+// https://github.com/domasx2/testing-library-selector
+import {byPlaceholderText} from 'testing-library-selector'
+
+const inputElement = byPlaceholderText('hello-placeholder')
+
+beforeEach(() => {
+  document.body.innerHTML = '<input type="text" placeholder="hello-placeholder" value="initial"/>'
+})
+
+it('read the initial value', () => {
+  expect(inputElement.get()).toHaveValue('initial')
+})
+
+it('read the initial value', async () => {
+  expect(await inputElement.find()).toHaveValue('initial')
+})
