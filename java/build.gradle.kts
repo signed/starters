63685b88b5f35dae860b3d48cb4591ab6dd2bb49
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2022-05-26 10:44:13 +0200

    upgrade jave setup

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -1,19 +1,18 @@
 plugins {
-    id("com.github.ben-manes.versions") version "0.28.0"
-    id("se.patrikerdes.use-latest-versions") version "0.2.13"
+    id("com.github.ben-manes.versions") version "0.42.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
     java
 }
 
 repositories {
     mavenCentral()
-    jcenter()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.6.2")
-    testImplementation("org.assertj:assertj-core:3.15.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0-M1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0-M1")
+    testImplementation("org.assertj:assertj-core:3.22.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0-M1")
 }
 
 tasks.test {
