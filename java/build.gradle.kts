3f4b97e9f9407f976ef6bbf648c371dc02efe4d8
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2022-05-26 10:49:13 +0200

    upgrade jave setup

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -9,10 +9,13 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0-M1")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0-M1")
+    testImplementation(platform("org.junit:junit-bom:5.8.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.8.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.22.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0-M1")
 }
 
 tasks.test {
