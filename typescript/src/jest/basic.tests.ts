d26ef19817c579f2d2f0a7f961a8c2c7ebd198a7
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2022-05-29 15:13:05 +0200

    improve setup

---
+++ typescript/src/jest/basic.tests.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@jest/globals'
+
+test('first', () => {
+  expect(1).toBe(1)
+})
+
+test('second', () => {
+  expect(1).toBe(1)
+})
--- typescript/src/jest/reporter.ts
+++ typescript/src/jest/reporter.ts
@@ -1,61 +1,75 @@
-import type { TestContext, Reporter, ReporterOnStartOptions, Test } from '@jest/reporters'
+import type { Reporter, ReporterOnStartOptions, Test, TestContext } from '@jest/reporters'
 import type { AggregatedResult, AssertionResult, TestResult } from '@jest/test-result'
-import { Config } from '@jest/types'
+import type { Config } from '@jest/types'
+
 // https://brunoscheufler.com/blog/2020-02-14-supercharging-jest-with-custom-reporters
 // Our reporter implements only the onRunComplete lifecycle
 // function, run after all tests have completed
 class CustomReporter implements Reporter {
-  messages: string[] = []
+  private _globalConfig: Config.GlobalConfig
+  private _options: unknown
+  private messages: string[] = []
 
-  constructor(private _config: Config.GlobalConfig, private _options: unknown) {
+  constructor(globalConfig: Config.GlobalConfig, options: unknown) {
+    this._globalConfig = globalConfig
+    this._options = options
     this.add('instance created')
   }
 
   // called
   onRunStart(_results: AggregatedResult, _options: ReporterOnStartOptions): void | Promise<void> {
     this.add('tracer: onRunStart')
+    return undefined
   }
 
   // called
   onTestFileStart(test: Test): void | Promise<void> {
     this.add(`tracer: onTestFileStart ${test.path}`)
+    return undefined
   }
 
+  // deprecated and will not be called if onTestFileStart exists
   onTestStart(_test: Test): void | Promise<void> {
     this.add('tracer: onTestStart')
+    return undefined
   }
 
   // called
-  onTestCaseResult(test: Test, testCaseResult: AssertionResult): void | Promise<void> {
+  onTestCaseResult(_test: Test, _testCaseResult: AssertionResult): void | Promise<void> {
     this.add('tracer: onTestCaseResult')
-    testCaseResult.status = 'failed'
+    return undefined
   }
 
-  // called
-  onTestFileResult(_test: Test, _testResult: TestResult, _aggregatedResult: AggregatedResult): void | Promise<void> {
+  //called
+  onTestFileResult(_test: Test, _testResult: TestResult, _aggregatedResult: AggregatedResult): Promise<void> | void {
     this.add('tracer: onTestFileResult')
+    return undefined
   }
 
+  // deprecated and will not be called if onTestFileResult exists
   onTestResult(test: Test, testResult: TestResult, aggregatedResult: AggregatedResult): void | Promise<void> {
     this.add('tracer: onTestResult')
     aggregatedResult.success = false
+    return undefined
   }
 
   //called
-  async onRunComplete(_: Set<TestContext>, _results: AggregatedResult) {
+  onRunComplete(_: Set<TestContext>, _results: AggregatedResult): Promise<void> | void {
     this.add('tracer: onRunComplete')
+    return undefined
   }
 
-  //called
+  //was not called :(
   getLastError(): void | Error {
     this.add('tracer: getLastError')
     console.log(this.messages.join('\n'))
     return new Error('You should not have done this')
+    //return undefined
   }
 
   protected add(message: string) {
     this.messages.push(message)
-    //console.log(message);
+    console.log(message)
   }
 }
 
