a88b0d285c43705cf2eccab1d2182c6e3fb8603c
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2022-05-29 11:18:08 +0200

    switch to pnpm

--- typescript/src/jest/reporter.ts
+++ typescript/src/jest/reporter.ts
@@ -1,39 +1,39 @@
-import type { Context, Reporter, ReporterOnStartOptions, Test } from '@jest/reporters';
-import type { AggregatedResult, AssertionResult, TestResult } from '@jest/test-result';
-import { Config } from '@jest/types';
+import type { TestContext, Reporter, ReporterOnStartOptions, Test } from '@jest/reporters'
+import type { AggregatedResult, AssertionResult, TestResult } from '@jest/test-result'
+import { Config } from '@jest/types'
 // https://brunoscheufler.com/blog/2020-02-14-supercharging-jest-with-custom-reporters
 // Our reporter implements only the onRunComplete lifecycle
 // function, run after all tests have completed
 class CustomReporter implements Reporter {
   messages: string[] = []
 
-  constructor(private config: Config.GlobalConfig, private options: unknown) {
+  constructor(private _config: Config.GlobalConfig, private _options: unknown) {
     this.add('instance created')
   }
 
   // called
-  onRunStart(results: AggregatedResult, options: ReporterOnStartOptions): void | Promise<void> {
-    this.add('tracer: onRunStart');
+  onRunStart(_results: AggregatedResult, _options: ReporterOnStartOptions): void | Promise<void> {
+    this.add('tracer: onRunStart')
   }
 
   // called
   onTestFileStart(test: Test): void | Promise<void> {
-    this.add(`tracer: onTestFileStart ${test.path}`);
+    this.add(`tracer: onTestFileStart ${test.path}`)
   }
 
-  onTestStart(test: Test): void | Promise<void> {
-    this.add('tracer: onTestStart');
+  onTestStart(_test: Test): void | Promise<void> {
+    this.add('tracer: onTestStart')
   }
 
   // called
   onTestCaseResult(test: Test, testCaseResult: AssertionResult): void | Promise<void> {
-    this.add('tracer: onTestCaseResult');
+    this.add('tracer: onTestCaseResult')
     testCaseResult.status = 'failed'
   }
 
   // called
-  onTestFileResult(test: Test, testResult: TestResult, aggregatedResult: AggregatedResult): void | Promise<void> {
-    this.add('tracer: onTestFileResult');
+  onTestFileResult(_test: Test, _testResult: TestResult, _aggregatedResult: AggregatedResult): void | Promise<void> {
+    this.add('tracer: onTestFileResult')
   }
 
   onTestResult(test: Test, testResult: TestResult, aggregatedResult: AggregatedResult): void | Promise<void> {
@@ -42,14 +42,14 @@ class CustomReporter implements Reporter {
   }
 
   //called
-  async onRunComplete(_: Set<Context>, results: AggregatedResult) {
-    this.add('tracer: onRunComplete');
+  async onRunComplete(_: Set<TestContext>, _results: AggregatedResult) {
+    this.add('tracer: onRunComplete')
   }
 
   //called
   getLastError(): void | Error {
-    this.add('tracer: getLastError');
-    console.log(this.messages.join('\n'));
+    this.add('tracer: getLastError')
+    console.log(this.messages.join('\n'))
     return new Error('You should not have done this')
   }
 
@@ -59,4 +59,4 @@ class CustomReporter implements Reporter {
   }
 }
 
-module.exports = CustomReporter;
+module.exports = CustomReporter
