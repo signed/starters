ae2d41394476845cc5c0aeebe01b845bbec3117e
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-10-22 21:30:35 +0200

    convert reusable-selectors tests to vitest

--- typescript/src/testing-library/reusable-selectors.vitest.ts
+++ typescript/src/testing-library/reusable-selectors.vitest.ts
@@ -1,4 +1,8 @@
-import { beforeEach, expect, it } from '@jest/globals'
+/**
+ * @vitest-environment jsdom
+ */
+import { beforeEach, expect, it } from 'vitest'
+import '@testing-library/jest-dom/vitest'
 // https://github.com/domasx2/testing-library-selector
 import { byPlaceholderText } from 'testing-library-selector'
 
