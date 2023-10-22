82693bb5396f37b58d2ea7e402f67c56a04bd296
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-10-22 21:42:11 +0200

    change test file extension back to tests

--- typescript/vitest.config.ts
+++ typescript/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['**/*.vitest.ts'],
+    include: ['**/*.tests.ts'],
     testTimeout: 1000,
   },
 })
