3eb76b575f25f11584017c082d036e65d8b8f25f
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-10-22 18:55:49 +0200

    convert mocking tests to vitest

--- typescript/src/jest/mocking.vitest.ts
+++ typescript/src/jest/mocking.vitest.ts
@@ -1,9 +1,9 @@
-import { expect, jest, test } from '@jest/globals'
+import { expect, vi, test } from 'vitest'
 
 const passErrorToCallback = (callback: (error: Error) => void) => callback(new Error('Secret Sauce'))
 
 test('message of error passed to the callback ', () => {
-  const callback = jest.fn()
+  const callback = vi.fn()
   passErrorToCallback(callback)
   const passedError: any = callback.mock.calls[0]?.[0]
   expect(passedError.message).toEqual('Secret Sauce')
