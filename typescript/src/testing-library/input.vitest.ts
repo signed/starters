879643786b0b791fe8600dcea3589a04bd951077
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-10-22 20:06:51 +0200

    structure test

--- typescript/src/testing-library/input.vitest.ts
+++ typescript/src/testing-library/input.vitest.ts
@@ -3,27 +3,25 @@
  */
 // https://testing-library.com/docs/dom-testing-library/cheatsheet
 // https://testing-library.com/docs/user-event/intro/#writing-tests-with-userevent
-import { beforeEach, describe, expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import '@testing-library/jest-dom/vitest'
 import { fireEvent, queries } from '@testing-library/dom'
 import { userEvent } from '@testing-library/user-event'
 
 describe('text input', () => {
-  beforeEach(() => {
-    document.body.innerHTML = '<input type="text" placeholder="hello-placeholder" value="initial"/>'
-  })
-
   it('read the initial value', () => {
+    const { inputElement } = renderInput()
     expect(inputElement()).toHaveValue('initial')
   })
 
   it('change the initial value', () => {
+    const { inputElement } = renderInput()
     fireEvent.change(inputElement(), { target: { value: 'updated' } })
     expect(inputElement()).toHaveValue('updated')
   })
 
   it('text is always appended currently there seems to be no way to position the caret or do a selection', async () => {
-    const user = userEvent.setup()
+    const { user, inputElement } = renderInput()
     inputElement().value = ''
     await user.type(inputElement(), 'one')
     await user.type(inputElement(), 'two')
@@ -31,5 +29,13 @@ describe('text input', () => {
   })
 })
 
-const inputElement = (): HTMLInputElement =>
-  queries.getByPlaceholderText(document.body, 'hello-placeholder') as HTMLInputElement
+const renderInput = () => {
+  const user = userEvent.setup()
+  document.body.innerHTML = '<input type="text" placeholder="hello-placeholder" value="initial"/>'
+  const inputElement = (): HTMLInputElement =>
+    queries.getByPlaceholderText(document.body, 'hello-placeholder') as HTMLInputElement
+  return {
+    user,
+    inputElement,
+  }
+}
