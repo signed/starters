fb337c490a6cefbdf97e169eb012a451bf99d7e8
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-01-19 19:18:37 +0100

    fix warning in gradle

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.assertj:assertj-core:3.25.1")
 }
 
