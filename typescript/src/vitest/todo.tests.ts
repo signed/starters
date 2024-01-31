d1f8d642a1492ccb65cca320b8d290ccfee08966
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-01-31 18:31:07 +0100

    bare bones starter

--- typescript/src/vitest/todo.tests.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from 'vitest'
-
-describe('just want to keep a reminder to write a test', () => {
-  test.todo('this feature needs this test')
-  test('already implemented test', () => {
-    expect(true).toBe(true)
-  })
-})
