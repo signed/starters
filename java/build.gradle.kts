580b32950cf6db734cabfda0b6c0a7b24e82d624
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-11-08 21:57:23 +0100

    update dependencies

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -9,10 +9,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.11.0-M1"))
+    testImplementation(platform("org.junit:junit-bom:5.11.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation("org.assertj:assertj-core:3.26.3")
 }
 
 tasks.test {
